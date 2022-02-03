ced092c103aee57adaa54fbd7f2bd54f0963bb14
danstd <danielst.davis@gmail.com>
2022-02-03 17:08:45 -0500

    updated app file to get from local database
    Main app file no longer contains request to integrity api; now reads from local database. Currently, the index view is set to get the count of operations (companies) grouped by country and organic certification status. Renamed old app file to organic_integrity_request.py

--- organic.py
+++ organic.py
@@ -1,97 +1,129 @@
-
 from flask import Flask, redirect, render_template, request, url_for
 import requests
 import json
 from api_key_get import key_get
 import xml.etree.ElementTree as ET
 import xmltodict
-import integrity_model
+from collections import OrderedDict
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.debug = True
 
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="flaskuser",
+    password="dersAGef3rover",
+
+    hostname="127.0.0.1",
+    databasename="organic_integrity",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+#app.secret_key = ""
+#login_manager = LoginManager()
+#login_manager.init_app(app)
+#migrate = Migrate(app, db)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
-        # Opening JSON file
-        #with open('/home/ddavis11/mysite/fips.json', 'r') as f:
-        with open("fips.json", "r") as f:   
-            # Reading from json file
-            fips_return = json.load(f)
-
-        return render_template("main_page.html", fips_display=fips_return)
-
-    # Get fips codes for states and territories
-    # with open('/home/ddavis11/mysite/fips.json', 'r') as f:
-        # fips_return = json.load(f)
-
-    # Get selected state fips code from form
-    selected_fips = request.form.get("states")
-
-    # JSON file for testing
-    # with open('/home/ddavis11/mysite/result.json', 'r') as f:
-     #   request_return = json.load(f)
-
-    # return render_template("main_page.html", return_display=census_request(selected_fips), fips_display=fips_return, org_display = integrity_request)
-    return render_template("main_page.html", organic_display = integrity_request())
-
-
-def integrity_request():
-    MY_KEY = key_get("integrity")
-    base_url = "https://organicapi.ams.usda.gov/IntegrityPubDataServices/OidPublicDataService.svc/rest/Operations?api_key=" + MY_KEY
-    # end_point = base_url + "?" + get_string + "&" + for_pred
-    end_point = base_url
-    # res = requests.get(end_point)
-    proxy_server = "http://proxy.server:3128"
-
-    contents = { "startIdx":"1", "count":"5", "countries":["BEL"], "states":["VA", "CA"] }
-    res = requests.post(end_point, json=contents, proxies = { "http" : proxy_server})
-    # check for valid request
-    # print(f"status code: {res.status_code}")
-    if res.status_code != 200:
-        return "ERROR: API request unsuccessful."
+        return render_template("main_page.html")
     else:
-        #return res.text
-        # XML handling from https://primates.dev/how-to-parse-an-xml-with-python/
-        #root = ET.fromstring(res.content)
-        #print(res.content)
-        #xmlDict = {}
-        #for sitemap in root:
-        #    children = list(sitemap)
-        #    if len(children) > 1:
-        #        xmlDict[children[0].text] = children[1].text
-        #return xmlDict
-        dict_data = xmltodict.parse(res.content)
-        print(dict_data.keys())
-        return dict_data
+        # Query to get number of operations by status and by country
+        op_return = db.session.query(
+            OrganicOperation.opPA_country,
+            OrganicOperation.op_status,
+            db.func.count(OrganicOperation.op_nopOpID)
+        ).select_from(OrganicOperation).order_by(OrganicOperation.opPA_country).group_by(
+            OrganicOperation.opPA_country,
+            OrganicOperation.op_status,)
 
 
-def census_request(fips_selection=None):
-    get_list = [
-        "Aggregate_HH_INC_ACS_14_18",
-        "Aggr_House_Value_ACS_14_18",
-        "Owner_Occp_HU_ACS_14_18",
-        "avg_Agg_House_Value_ACS_14_18",
-        "Tot_Occp_Units_ACS_14_18",
-        "pct_Owner_Occp_HU_ACS_14_18"
-    ]
+        return render_template(
+            "main_page.html",
+            organic_display=op_return
+        )
 
-    get_string = "get=" + ",".join(get_list)
+# Models
+class OrganicOperation(db.Model):
+    __tablename__ = "organic_operation"
+    op_certifierName = db.Column(db.String(256))
+    op_nopOpID = db.Column(db.String(256), primary_key=True)
+    op_name = db.Column(db.String(256))
+    op_otherNames = db.Column(db.String(256))
+    op_clientID = db.Column(db.String(256))
+    op_contFirstName = db.Column(db.String(256))
+    op_contLastName = db.Column(db.String(256))
+    op_status = db.Column(db.String(256))
+    op_statusEffectiveDate = db.Column(db.DateTime)
+    op_nopAnniversaryDate = db.Column(db.DateTime)
+    op_lastUpdatedDate = db.Column(db.DateTime)
+    opSC_CR = db.Column(db.String(256))
+    opSC_CR_ED = db.Column(db.DateTime)
+    opSC_LS = db.Column(db.String(256))
+    opSC_LS_ED = db.Column(db.DateTime)
+    opSC_WC = db.Column(db.String(256))
+    opSC_WC_ED = db.Column(db.DateTime)
+    opSC_HANDLING = db.Column(db.String(256))
+    opSC_HANDLING_ED = db.Column(db.DateTime)
+    opPA_line1 = db.Column(db.String(256))
+    opPA_line2 = db.Column(db.String(256))
+    opPA_city = db.Column(db.String(256))
+    opPA_state = db.Column(db.String(256))
+    opPA_country = db.Column(db.String(256))
+    opPA_zip = db.Column(db.String(256))
+    opPA_countyCode = db.Column(db.String(256))
+    opPA_county = db.Column(db.String(256))
+    opMA_line1 = db.Column(db.String(256))
+    opMA_line2 = db.Column(db.String(256))
+    opMA_city = db.Column(db.String(256))
+    opMA_state = db.Column(db.String(256))
+    opMA_country = db.Column(db.String(256))
+    opMA_zip = db.Column(db.String(256))
+    opMA_countyCode = db.Column(db.String(256))
+    opMA_county = db.Column(db.String(256))
+    op_phone = db.Column(db.String(256))
+    op_email = db.Column(db.String(256))
+    op_url = db.Column(db.String(256))
+    op_opExtraInfo = db.Column(db.String(256))
+    opEx_broker = db.Column(db.String(256))
+    opEx_csa = db.Column(db.String(256))
+    opEx_copacker = db.Column(db.String(256))
+    opEx_dairy = db.Column(db.String(256))
+    opEx_distributor = db.Column(db.String(256))
+    opEx_marketerTrader = db.Column(db.String(256))
+    opEx_restaurant = db.Column(db.String(256))
+    opEx_retailer = db.Column(db.String(256))
+    opEx_poultry = db.Column(db.String(256))
+    opEx_privateLabeler = db.Column(db.String(256))
+    opEx_slaughterHouse = db.Column(db.String(256))
+    opEx_storage = db.Column(db.String(256))
+    opEx_growerGroup = db.Column(db.String(256))
+    opCert_url = db.Column(db.String(256))
 
-    if fips_selection:
-        for_pred = "for=state:" + fips_selection
-    else:
-        for_pred = "for=state:*"
 
-    base_url = "https://api.census.gov/data/2020/pdb/statecounty"
-    end_point = base_url + "?" + get_string + "&" + for_pred
+class OrganicItem(db.Model):
+    __tablename__ = "organic_item"
+    ci_artID = db.Column(db.Integer, primary_key=True) # Artificial primary key added in.
+    ci_nopOpID = db.Column(db.String(256), db.ForeignKey(OrganicOperation.op_nopOpID))
+    ci_certNumber = db.Column(db.String(256))
+    ci_nopScope = db.Column(db.String(256))
+    ci_nopCategory = db.Column(db.String(256))
+    ci_nopCatID = db.Column(db.String(256))
+    ci_nopCatName = db.Column(db.String(256))
+    ci_nopItemID = db.Column(db.String(256))
+    ci_itemList = db.Column(db.String(256))
+    ci_varieties = db.Column(db.String(256))
+    ci_status = db.Column(db.String(256))
+    ci_statusEffectiveDate = db.Column(db.String(256))
+    ci_organic100 = db.Column(db.String(256))
+    ci_organic = db.Column(db.String(256))
+    ci_madeWithOrganic = db.Column(db.String(256))
+    fk_operation_ID = db.relationship("OrganicOperation", foreign_keys=ci_nopOpID)
 
-    res = requests.get(end_point)
 
-    # check for valid request
-    # print(f"status code: {res.status_code}")
-    if res.status_code != 200:
-        return "ERROR: API request unsuccessful."
-    else:
-        return res.json()
+
+
